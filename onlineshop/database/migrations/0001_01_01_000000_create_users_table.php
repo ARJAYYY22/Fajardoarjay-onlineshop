bb4ae50132782a153eb640faa778e51bbadb14be
Arjay Fajardo <fajardiarjay@gmail.com>
2025-03-27 09:44:37 +0800

    congrats man

--- onlineshop/database/migrations/0001_01_01_000000_create_users_table.php
+++ onlineshop/database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('Firstname');
+            $table->string('Middlename');
+            $table->string('Lastname');
+            $table->integer('usertype');
+            $table->string('Address');
+
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
